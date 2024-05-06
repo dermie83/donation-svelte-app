03ced5acfbc8fd45c92df324db39b389f0074e8f
dermie83 <dermiemadsen@gmail.com>
2024-05-06 18:35:41 +0100

    maps dev with Leaflet library

--- donation-svelte/src/routes/donate/+page.svelte
+++ donation-svelte/src/routes/donate/+page.svelte
@@ -9,30 +9,61 @@
     import { get } from "svelte/store";
     import type { Candidate, DataSet, Donation } from "$lib/types/donation-types";
     import { generateByCandidate } from "$lib/services/donation-utils";
+    import LeafletMap from "$lib/ui/LeafletMap.svelte";
+    import DonationList from "$lib/ui/DonationList.svelte";
+    
   
     let candidateList: Candidate[] = [];
     let donations: Donation[] = [];
     let donationsByCandidate: DataSet;
     let candidates: Candidate[] = [];
+    let map: LeafletMap;
   
     subTitle.set("Make a Donation");
   
     onMount(async () => {
-      candidateList = await donationService.getCandidates(get(currentSession));
-      donations = await donationService.getDonations(get(currentSession));
-      candidates = await donationService.getCandidates(get(currentSession));
-      donationsByCandidate = generateByCandidate(donations, candidates);
-    });
-  
-    latestDonation.subscribe(async (donation) => {
-      if (donation) {
-        donations.push(donation);
-        donations = [...donations];
-        donationsByCandidate = generateByCandidate(donations, candidates);
+    candidateList = await donationService.getCandidates(get(currentSession));
+    donations = await donationService.getDonations(get(currentSession));
+    candidates = await donationService.getCandidates(get(currentSession));
+    donationsByCandidate = generateByCandidate(donations, candidates);
+
+    donations.forEach((donation: Donation) => {
+      if (typeof donation.candidate !== "string") {
+        const popup = `${donation.candidate.firstName} ${donation.candidate.lastName}: €${donation.amount}`;
+        map.addMarker(donation.lat, donation.lng, popup);
       }
     });
-  </script>
+    const lastDonation = donations[donations.length - 1];
+    if (lastDonation) map.moveTo(lastDonation.lat, lastDonation.lng);
+  });
+
+  latestDonation.subscribe(async (donation) => {
+    if (donation) {
+      donations.push(donation);
+      donations = [...donations];
+      donationsByCandidate = generateByCandidate(donations, candidates);
+    }
+    if (typeof donation.candidate !== "string") {
+      const popup = `${donation.candidate.firstName} ${donation.candidate.lastName}: €${donation.amount}`;
+      map.addMarker(donation.lat, donation.lng, popup);
+      map.moveTo(donation.lat, donation.lng);
+    }
+
+  });
+</script>
   
+  <div class="columns">
+    <div class="column">
+      <Card title="Donatinons to Date">
+        <LeafletMap height={64.5} bind:this={map} />
+      </Card>
+    </div>
+    <div class="column">
+      <Card title="Please Donate">
+        <DonateForm {candidateList} />
+      </Card>
+    </div>
+  </div>
   <div class="columns">
     <div class="column">
       <Card title="Donatinons to Date">
@@ -41,7 +72,7 @@
     </div>
     <div class="column">
       <Card title="Please Donate">
-        <DonateForm {candidateList} />
+        <DonationList {donations} />
       </Card>
     </div>
   </div>
